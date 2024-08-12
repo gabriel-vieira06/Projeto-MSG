df56f537fbf10c822ad4fe1c1e55ea5184563944
Gabriel Vieira <gabrielvieira.cavalcante@gmail.com>
2024-08-12 14:50:35 -0300

    add remote server init

--- server.py
+++ server.py
@@ -2,29 +2,48 @@ import Pyro4
 import Pyro4.naming
 import threading
 import tkinter as tk
-from tkinter import messagebox
+
+class RemoteServer:
+    def __init__(self):
+        self.nome   = None
 
 class NameServer:
-    def name_server_init(self, ip):
-        t_name_server = threading.Thread(
-            target=Pyro4.naming.startNSloop, kwargs={"host": ip}, daemon=True
-        )
-        t_name_server.start()
-
-class NameServerInterface:
     def __init__(self):
-        self.ip = None
+        self.ip     = None
         self.window = None
 
     def get_ip(self):
         self.ip = entry.get()
         if self.ip:
-            name_server = NameServer()
-            name_server.name_server_init(self.ip)
+            self.launch_name_server()
+            self.launch_remote_server()
         else:
             print("ERRO: ENTRADA VAZIA")
 
-nameServerGui = NameServerInterface()
+    def launch_name_server(self):
+        threadNameServer = threading.Thread(
+            target=Pyro4.naming.startNSloop, kwargs={"host": self.ip}, daemon=True
+        )
+        threadNameServer.start()
+
+    def launch_remote_server(self):
+        threadRemoteServer = threading.Thread(
+            target=start_remote_server, kwargs={"ip": self.ip}, daemon=True
+        )
+        threadRemoteServer.start()
+        
+def start_remote_server(ip):
+    daemon  = Pyro4.Daemon(host=ip)
+    try:
+        ns  = Pyro4.locateNS(host=ip, port=9090)
+        uri = daemon.register(RemoteServer)
+        ns.register("chat.server", uri)
+        print("Aguardando Conexões...")
+        daemon.requestLoop()
+    except Exception as e:
+        print(e)
+
+nameServerGui = NameServer()
 
 nameServerGui.window = tk.Tk()
 nameServerGui.window.title("IP - Servidor de Nomes")
@@ -35,8 +54,8 @@ entry = tk.Entry(nameServerGui.window, width=40)
 entry.pack(pady=10)
 
 # Botão para submeter a entrada
-button_submit = tk.Button(nameServerGui.window, text="Enviar", command=nameServerGui.get_ip)
-button_submit.pack(pady=5)
+buttonSubmit = tk.Button(nameServerGui.window, text="Enviar", command=nameServerGui.get_ip)
+buttonSubmit.pack(pady=5)
 
 # Loop principal
 nameServerGui.window.mainloop()
